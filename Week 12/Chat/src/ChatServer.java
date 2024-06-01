9c5c530a33e3643ae0db8fab43e668b6d114ddce
Dadhija <73417020+Dadhija@users.noreply.github.com>
2024-06-01 11:26:09 -0400

    Add files via upload

---
+++ Week 12/Chat/src/ChatServer.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class ChatServer {
+    private int port;
+    private Set<String> userNames = new HashSet<>();
+    private Set<UserThread> userThreads = new HashSet<>();
+    String serverName = "Dadhija's server";
+
+    public ChatServer(int port, String serverName) {
+        this.port = port;
+        this.serverName = serverName;
+    }
+
+    public void execute() {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Chat Server (" + serverName + ") is listening on port " + port);
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New user connected");
+
+                UserThread newUser = new UserThread(socket, this);
+                userThreads.add(newUser);
+                newUser.start();
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Error in the server: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
+    void broadcast(String message, UserThread excludeUser) {
+        for (UserThread aUser : userThreads) {
+            if (aUser != excludeUser) {
+                aUser.sendMessage(message);
+            }
+        }
+    }
+
+    void addUserName(String userName) {
+        userNames.add(userName);
+    }
+
+    void removeUser(String userName, UserThread aUser) {
+        boolean removed = userNames.remove(userName);
+        if (removed) {
+            userThreads.remove(aUser);
+            System.out.println("The user " + userName + " quitted");
+        }
+    }
+
+    Set<String> getUserNames() {
+        return this.userNames;
+    }
+
+    String getServerName() {
+        return serverName;
+    }
+
+    boolean hasUsers() {
+        return !this.userNames.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Syntax: java ChatServer <port-number>");
+            System.exit(0);
+        }
+
+        int port = Integer.parseInt(args[0]);
+        
+
+        ChatServer server = new ChatServer(port, "Dadhija's server");
+        server.execute();
+    }
+}
